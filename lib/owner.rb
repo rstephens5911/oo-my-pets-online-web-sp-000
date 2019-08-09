a57d5ffde0918f22301ce0f1400ec376cc65025f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-09 21:35:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,7 +67,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{self.dogs.size} dog(s), and #{Cat.all.size}, cat(s)."
+    "I have #{self.dogs.size} dog(s), and #{self.all.size}, cat(s)."
   end
 
 
